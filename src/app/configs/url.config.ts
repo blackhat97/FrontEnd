dbee2c6a404dc0281fc2f35d4d82c3bcaa8a3f73
Johnny Park <johnnypark0201@gmail.com>
2020-05-06 19:02:28 +0900

    http to https

--- src/app/configs/url.config.ts
+++ src/app/configs/url.config.ts
@@ -1,6 +1,6 @@
 
 //export const apiURL = window.location.host.includes('localhost') ? 'http://localhost:3000' : 'https://api.webcomics.pk';
-export const comicURL = 'http://api.webcomics.pk/comic';
-export const searchURL = 'http://api.webcomics.pk/search/comic';
-export const genreURL = 'http://api.webcomics.pk/genre';
-export const imageURL = 'http://api.webcomics.pk/upload';
+export const comicURL = 'https://api.webcomics.pk/comic';
+export const searchURL = 'https://api.webcomics.pk/search/comic';
+export const genreURL = 'https://api.webcomics.pk/genre';
+export const imageURL = 'https://api.webcomics.pk/upload';
